77d191dd692e41a5cbf02c38c7f6a2821252cae3
luckynguyen1995 <tg.luckynguyen.1995@gmail.com>
2017-08-04 02:30:43 +0700

    Add project files.

---
+++ src/WebAPI/Startup.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using WebAPI.Models;
+namespace WebAPI
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<TodoContext>(opt => opt.UseInMemoryDatabase());
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseMvc();
+        }
+    }
+}
